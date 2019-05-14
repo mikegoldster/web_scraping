18716d35099e816f3c309d705957b85626b84ce8
mikegoldster <45051483+mikegoldster@users.noreply.github.com>
2019-05-14 14:24:29 -0500

    Add files via upload

--- mars_app.py
+++ mars_app.py
@@ -6,37 +6,38 @@ import scrape_mars
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
+
+db = client.mars_db
+collection = db.mars_data_coll
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
 
     # Find one record of data from the mongo database
-    destination_data = mongo.db.collection.find_one()
+    mars_mission_data = mars_data_coll.find_one()
 
     # Return template and data
-    return render_template("index.html", vacation=destination_data)
-
+    return render_template("index.html", mars_info=mars_mission_data)
 
 # Route that will trigger the scrape function
 @app.route("/scrape")
 def scrape():
 
-    # Run the scrape function
-    def scrape():
-
     # Run scrapped functions
-    mars_info = mongo.db.mars_info
-    mars_data = scrape_mars.news_title()
-    mars_data = scrape_mars.news_p()
-    mars_data = scrape_mars.featured_image_url()
-    mars_data = scrape_mars.mars_weather()
-    mars_data = scrape_mars.facts_table()
-    mars_data = scrape_mars.hemisphere_image_urls()
+    # ? mars_results = mongo.db.mars_info
+    mars_info = scrape_mars.scrape_info()
+    mars_info = scrape_mars.news_title()
+    mars_info = scrape_mars.news_p()
+    mars_info = scrape_mars.featured_image_url()
+    mars_info = scrape_mars.mars_weather()
+    mars_info = scrape_mars.facts_table()
+    mars_info = scrape_mars.hemisphere_image_urls()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, mars_data, upsert=True)
+    mongo.db.collection.update({}, mars_info, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
